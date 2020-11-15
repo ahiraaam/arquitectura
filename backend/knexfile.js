16803c6e32b1e7fa21510466742dac58e589f139
ahiraaam <ahicruma23@gmail.com>
2020-11-15 16:10:53 -0600

    mysql

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,40 +1,40 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 module.exports = {
   development: {
-    client: 'mysql2',
+    client: "mysql",
     connection: {
-      host: process.env.DB_DEVELOPMENT_HOST || 'localhost',
-      port: process.env.DB_DEVELOPMENT_PORT || '3306',
-      database: process.env.DB_DEVELOPMENT_NAME || 'my_database',
-      user:  process.env.DB_DEVELOPMENT_USER || 'root',
-      password: process.env.DB_DEVELOPMENT_PASSWORD || ''
+      host: process.env.DB_DEVELOPMENT_HOST || "localhost",
+      port: process.env.DB_DEVELOPMENT_PORT || "3306",
+      database: process.env.DB_DEVELOPMENT_NAME || "my_database",
+      user: process.env.DB_DEVELOPMENT_USER || "root",
+      password: process.env.DB_DEVELOPMENT_PASSWORD || "",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: "knex_migrations",
+    },
   },
   production: {
-    client: 'mysql2',
+    client: "mysql",
     connection: {
-      host: process.env.DB_PRODUCTION_HOST || 'localhost',
-      port: process.env.DB_PRODUCTION_PORT || '3306',
-      database: process.env.DB_PRODUCTION_NAME || 'my_database',
-      user:  process.env.DB_PRODUCTION_USER || 'root',
-      password: process.env.DB_PRODUCTION_PASSWORD || ''
+      host: process.env.DB_PRODUCTION_HOST || "localhost",
+      port: process.env.DB_PRODUCTION_PORT || "3306",
+      database: process.env.DB_PRODUCTION_NAME || "my_database",
+      user: process.env.DB_PRODUCTION_USER || "root",
+      password: process.env.DB_PRODUCTION_PASSWORD || "",
     },
     pool: {
       min: 2,
-      max: 10
+      max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
+      tableName: "knex_migrations",
+    },
+  },
 };
--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "*",
     credentials: true,
   })
 );
